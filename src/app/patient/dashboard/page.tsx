ab7ed25434c5488a58e8e21ef025f6c71c7fef76
Joshua Elangwe <joshuaelangwe12@gmail.com>
2025-09-23 01:59:45 +0000

    Try fixing this error: `Console Error: Only plain objects can be passed

--- src/app/patient/dashboard/page.tsx
+++ src/app/patient/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type { NavItem } from "@/types";
